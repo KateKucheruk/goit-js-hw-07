2bda448cca590f25ac78ee622a10ae8946138767
KateKucheruk <ki290489kea@gmail.com>
2024-08-02 19:23:30 +0300

    add one

---
+++ js/task-3.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const inputElement = document.getElementById("name-input");
+  const outputElement = document.getElementById("name-output");
+
+  inputElement.addEventListener("input", () => {
+    const trimmedValue = inputElement.value.trim();
+
+    if (trimmedValue === "") {
+      outputElement.textContent = "Anonymous";
+    } else {
+      outputElement.textContent = trimmedValue;
+    }
+  });
+});
---
+++ js/task-6.js
@@ -0,0 +1,62 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function createBoxes(amount) {
+  const boxesContainer = document.getElementById("boxes");
+  const boxes = [];
+  let size = 30;
+
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    box.classList.add("box");
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxes.push(box);
+    size += 10;
+  }
+
+  boxesContainer.append(...boxes);
+}
+
+function destroyBoxes() {
+  const boxesContainer = document.getElementById("boxes");
+  boxesContainer.innerHTML = "";
+}
+
+document.querySelector("[data-create]").addEventListener("click", () => {
+  const input = document.querySelector("#controls input");
+  const amount = Number(input.value);
+
+  if (amount >= 1 && amount <= 100) {
+    destroyBoxes();
+    createBoxes(5);
+    input.value = "";
+  } else {
+    alert("Будь ласка, введіть число від 1 до 100.");
+  }
+});
+
+document.querySelector("[data-destroy]").addEventListener("click", () => {
+  destroyBoxes();
+});
+
+const createButton = document.querySelector("button[data-create]");
+const destroyButton = document.querySelector("button[data-destroy]");
+
+function styleButtonBackground(button, backgroundColor) {
+  button.style.backgroundColor = backgroundColor;
+}
+
+styleButtonBackground(createButton, "#4E75FF");
+styleButtonBackground(destroyButton, "#FF4E4E");
+
+function styleButtonWidth(button, width) {
+  button.style.width = width;
+}
+
+styleButtonWidth(createButton, "120px");
+styleButtonWidth(destroyButton, "120px");
